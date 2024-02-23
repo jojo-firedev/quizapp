af71ec80f9e854dbab36fa2e9a60b5712622f847
Jonas Hofses <jonas@firedev.tech>
2024-02-23 13:40:39 +0100

    add csv service with adding dependencies

---
+++ lib/service/csv_service.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv.dart';
+import 'package:quizapp/models/jugendfeuerwehr.dart';
+
+class CsvService {
+  Future<List<Jugendfeuerwehr>> readCsv() async {
+    print('Reading CSV');
+    final input = File(
+            'C:\\Users\\jonas.hofses\\dev\\quizapp\\lib\\data\\jugendfeuerwehre-lklg.csv')
+        .openRead();
+    List<dynamic> fields = await input
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter())
+        .toList();
+    // fields = fields.removeAt(0);
+    List<Jugendfeuerwehr> list = fields
+        .map((e) => Jugendfeuerwehr(
+              name: e[0],
+              ort: e[1],
+              gemeinde: e[2],
+            ))
+        .toList();
+
+    print(list.first.gemeinde.toString());
+    return list;
+  }
+}
