a6ab7e7c12e7fabe25d4690f3f6f81d8f83ed598
Jonas Hofses <jonas@firedev.tech>
2024-02-23 13:41:02 +0100

    Hinzufügen der Jugendfeuerwehr-Klasse

---
+++ lib/models/jugendfeuerwehr.dart
@@ -0,0 +1,11 @@
+class Jugendfeuerwehr {
+  final String name;
+  final String ort;
+  final String gemeinde;
+
+  Jugendfeuerwehr({
+    required this.name,
+    required this.ort,
+    required this.gemeinde,
+  });
+}
