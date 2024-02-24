20701476b5d24e2d7c12d04306c7a34d70fa253f
Jonas <jonas@firedev.tech>
2024-02-24 15:23:09 +0100

    Hinzufügen der Startseite für die Anwendung

---
+++ lib/presentation/home/home_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: [
+          InkWell(
+            onTap: () => Navigator.pushNamed(context, '/quiz'),
+            child: const Card(
+              child: Center(
+                child: Text('Start Quiz'),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () => Navigator.pushNamed(context, '/teilnehmer'),
+            child: const Card(
+              child: Center(
+                child: Text('Teilnehmer Übersicht'),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: const Card(
+              child: Center(
+                child: Text(''),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: const Card(
+              child: Center(
+                child: Text(''),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: const Card(
+              child: Center(
+                child: Text(''),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: const Card(
+              child: Center(
+                child: Text(''),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
