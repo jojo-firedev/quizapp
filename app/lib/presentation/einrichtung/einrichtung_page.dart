e87d7ca232177c80ff25ca999c8db5a2ee0923d5
Jonas <jonas@firedev.tech>
2024-02-25 18:06:33 +0100

    Hinzufügen der Einrichtungsseite

---
+++ app/lib/presentation/einrichtung/einrichtung_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EinrichtungPage extends StatelessWidget {
+  const EinrichtungPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
