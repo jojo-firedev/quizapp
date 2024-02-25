93978eaeaebaa1d999ed02b2725c43b9f1028704
Jonas <jonas@firedev.tech>
2024-02-25 18:08:22 +0100

    Aktualisiere Teilnehmerübersichtsseite

--- app/lib/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart
+++ app/lib/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart
@@ -7,29 +7,31 @@ class TeilnehmerUebersichtPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Jugendfeuerwehr>>(
-      future: csvService.readCsv(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          final data = snapshot.data!;
-          return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              final gemeinde = data[index].gemeinde;
-              final ort = data[index].ort;
-              final name = data[index].name;
-              return ListTile(
-                title: Text(name),
-                subtitle: Text('$gemeinde, $ort'),
-              );
-            },
-          );
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
+    return Scaffold(
+      body: FutureBuilder<List<Jugendfeuerwehr>>(
+        future: csvService.readCsv(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (context, index) {
+                final gemeinde = data[index].gemeinde;
+                final ort = data[index].ort;
+                final name = data[index].name;
+                return ListTile(
+                  title: Text(name),
+                  subtitle: Text('$gemeinde, $ort'),
+                );
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
     );
   }
 }
