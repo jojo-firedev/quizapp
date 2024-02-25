89b560a4d786090663d4d03ec3797f3331126824
Jonas <jonas@firedev.tech>
2024-02-25 18:06:52 +0100

    Aktualisiere BuzzerService mit IP-Konfiguration

--- app/lib/service/buzzer_service.dart
+++ app/lib/service/buzzer_service.dart
@@ -4,10 +4,13 @@ import 'package:quizapp/service/network_service.dart';
 
 class BuzzerService {
   final NetworkService _networkService = NetworkService();
+
   void sendConfigWithIP() async {
     String ipAddress = await _networkService.getIpAddress();
+    print('IP: $ipAddress');
 
     String configMessage = '{"Config": {"ServerIPAdresse" : "$ipAddress"}}';
+    print('Config: $configMessage');
     var sender = await UDP.bind(Endpoint.any(port: const Port(65000)));
     var dataLength = await sender.send(
         configMessage.codeUnits, Endpoint.broadcast(port: const Port(8090)));
