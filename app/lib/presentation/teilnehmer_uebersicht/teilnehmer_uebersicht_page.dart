743a4b05cc67dbffc2244dc3e591d6bcc518d143
Jonas <jonas@firedev.tech>
2024-02-26 22:03:37 +0100

    Hinzufügen der Gruppierung von Daten nach Gemeinde in der Teilnehmerübersichtsseite

--- app/lib/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart
+++ app/lib/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:quizapp/models/jugendfeuerwehr.dart';
 import 'package:quizapp/service/csv_service.dart';
@@ -16,18 +17,38 @@ class TeilnehmerUebersichtPage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final data = snapshot.data!;
+            final groupedData =
+                groupBy(data, (Jugendfeuerwehr j) => j.gemeinde);
             return ListView.builder(
-              itemCount: data.length,
+              itemCount: groupedData.keys.length,
               itemBuilder: (context, index) {
-                final gemeinde = data[index].gemeinde;
-                final ort = data[index].ort;
-                final name = data[index].name;
-                return ListTile(
-                  title: Text(name),
-                  subtitle: Text('$gemeinde, $ort'),
+                final gemeinde = groupedData.keys.elementAt(index);
+                final feuerwehren = groupedData[gemeinde];
+                return ExpansionTile(
+                  title: Text(gemeinde),
+                  children: feuerwehren!
+                      .map((f) => ListTile(
+                            title: Text(f.name),
+                            // subtitle: Text('Ort: ${f.ort}'),
+                          ))
+                      .toList(),
                 );
               },
             );
+            // if (snapshot.hasData) {
+            //   final data = snapshot.data!;
+            //   return ListView.builder(
+            //     itemCount: data.length,
+            //     itemBuilder: (context, index) {
+            //       final gemeinde = data[index].gemeinde;
+            //       final ort = data[index].ort;
+            //       final name = data[index].name;
+            //       return ListTile(
+            //         title: Text(name),
+            //         subtitle: Text('Gemeinde/Stadt: $gemeinde'),
+            //       );
+            //     },
+            //   );
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           } else {
