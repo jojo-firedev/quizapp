f7da9cf8dd778848c89e070001dfbc2edb6c485e
Jonas <jonas@firedev.tech>
2024-02-26 18:25:46 +0100

    Aktualisiere die Home-Seite mit neuen Funktionen und Layout-Anpassungen

--- app/lib/presentation/home/home_page.dart
+++ app/lib/presentation/home/home_page.dart
@@ -14,7 +14,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    _buzzerSocketService = BuzzerSocketService();
+    _buzzerSocketService = BuzzerSocketService(context);
     super.initState();
   }
 
@@ -28,66 +28,183 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        return GridView.count(
           crossAxisCount: 3,
+          childAspectRatio:
+              MediaQuery.of(context).size.width / constraints.maxHeight,
           crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
+          children: [
+            Card(
+              child: InkWell(
+                onTap: () {},
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.fast_forward,
+                          size: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Geführter Modus',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () => Navigator.pushNamed(context, '/quiz'),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.quiz,
+                          size: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Start Quiz',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: InkWell(
+                onTap: () => Navigator.pushNamed(context, '/teilnehmer'),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.group,
+                          size: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Teilnehmer Übersicht',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: InkWell(
+                onTap: () => testBuzzer(),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.send_time_extension,
+                          color: Colors.white,
+                          size: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Send UDP Config',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: InkWell(
+                onTap: () =>
+                    _buzzerSocketService.lockBuzzer('00:00:00:00:00:00'),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.lock,
+                          color: Colors.white,
+                          size: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Lock all Buzzers',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: InkWell(
+                onTap: () => _buzzerSocketService.releaseBuzzer(),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Icon(
+                          Icons.lock_open,
+                          color: Colors.white,
+                          size: MediaQuery.of(context).size.width / 10,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Text(
+                          'Unlock all Buzzers',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      }),
+      floatingActionButton: Container(
+        padding: const EdgeInsets.all(20),
+        child: StreamBuilder<int>(
+          stream: _buzzerSocketService.connectedSocketsCountStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(
+                'Verbundene Buttons: ${snapshot.data}',
+                style: TextStyle(fontSize: 20),
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
         ),
-        children: [
-          InkWell(
-            onTap: () {},
-            child: const Card(
-              child: Center(
-                child: Text('Geführte Bedienung'),
-              ),
-            ),
-          ),
-          Card(
-            child: InkWell(
-              onTap: () => Navigator.pushNamed(context, '/quiz'),
-              child: const Center(
-                child: Text('Start Quiz'),
-              ),
-            ),
-          ),
-          Card(
-            child: InkWell(
-              onTap: () => Navigator.pushNamed(context, '/teilnehmer'),
-              child: const Center(
-                child: Text('Teilnehmer Übersicht'),
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.red,
-            child: InkWell(
-              onTap: () => testBuzzer(),
-              child: const Center(
-                child: Text('Send UDP Config',
-                    style: TextStyle(color: Colors.white)),
-              ),
-            ),
-          ),
-          Card(
-            color: Colors.red,
-            child: InkWell(
-              onTap: () {},
-              child: const Center(
-                child: Text('Send TCP Message',
-                    style: TextStyle(color: Colors.white)),
-              ),
-            ),
-          ),
-          Card(
-            child: InkWell(
-              onTap: () {},
-              child: const Center(
-                child: Text(''),
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
