9c43403ec59f7802a49590642d920466aed11ed7
Jonas <jonas@firedev.tech>
2024-02-26 18:01:55 +0100

    Aktualisiere BuzzerSocketService-Klasse

--- app/lib/service/buzzer_socket_service.dart
+++ app/lib/service/buzzer_socket_service.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 class BuzzerSocketService {
   List<Socket> _sockets = [];
+  List<String> _macs = [];
   ServerSocket? _serverSocket;
 
   BuzzerSocketService() {
@@ -11,7 +12,7 @@ class BuzzerSocketService {
 
   void _startServer() async {
     try {
-      _serverSocket = await ServerSocket.bind(InternetAddress.anyIPv4, 12345);
+      _serverSocket = await ServerSocket.bind(InternetAddress.anyIPv4, 8082);
       print('Server started on port ${_serverSocket!.port}');
       _serverSocket!.listen((Socket clientSocket) {
         _handleClient(clientSocket);
@@ -28,9 +29,16 @@ class BuzzerSocketService {
 
     clientSocket.listen((List<int> data) {
       String message = utf8.decode(data);
+      Map<String, dynamic> jsonObject = jsonDecode(message);
+      if (jsonObject.values.first == 'Connected') {
+        String mac = jsonObject.keys.first;
+        _macs.add(mac);
+      } else if (jsonObject.values.first == 'ButtonPressed') {
+        String mac = jsonObject.keys.first;
+        lockBuzzer(mac);
+      }
       print(
           'Received message from ${clientSocket.remoteAddress}:${clientSocket.remotePort}: $message');
-      _sendMessageToAll(message, clientSocket);
     }, onError: (error) {
       print('Error with client: $error');
       _sockets.remove(clientSocket);
@@ -41,12 +49,26 @@ class BuzzerSocketService {
     });
   }
 
-  void _sendMessageToAll(String message, Socket senderSocket) {
+  void _sendMessageToAll(String message, {Socket? senderSocket}) {
     for (var socket in _sockets) {
-      if (socket != senderSocket) {
-        socket.write(message);
+      if (socket == senderSocket && senderSocket != null) {
+        continue;
       }
+      socket.write(message);
     }
+  }
+
+  void lockBuzzer(String winnerMac) {
+    String lockMessage = jsonEncode({
+      'ButtonLock': [winnerMac]
+    });
+
+    _sendMessageToAll(lockMessage);
+  }
+
+  void releaseBuzzer() {
+    String resetMessage = jsonEncode({'ButtonRelease': []});
+    _sendMessageToAll(resetMessage);
   }
 
   void close() {
