f22e1744421bef3b594b53815bc3612c4d270657
Jonas <jonas@firedev.tech>
2024-02-26 22:56:17 +0100

    Füge BuzzerConfigPage hinzu

---
+++ app/lib/presentation/einrichtung/buzzer_config/2_buzzer_config_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BuzzerConfigPage extends StatefulWidget {
+  const BuzzerConfigPage({super.key});
+
+  @override
+  State<BuzzerConfigPage> createState() => _BuzzerConfigPageState();
+}
+
+class _BuzzerConfigPageState extends State<BuzzerConfigPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
