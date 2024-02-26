3017e3471fc75c8c1c8bb341b14f05c6a8589d86
Jonas <jonas@firedev.tech>
2024-02-26 22:56:45 +0100

    Teilnehmer übersichtsseite wiederstellen

--- app/lib/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart
+++ app/lib/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart
@@ -4,7 +4,9 @@ import 'package:quizapp/models/jugendfeuerwehr.dart';
 import 'package:quizapp/service/csv_service.dart';
 
 class TeilnehmerUebersichtPage extends StatelessWidget {
-  final CsvService csvService = CsvService();
+  final CsvService csvService = const CsvService();
+
+  const TeilnehmerUebersichtPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,38 +19,18 @@ class TeilnehmerUebersichtPage extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             final data = snapshot.data!;
-            final groupedData =
-                groupBy(data, (Jugendfeuerwehr j) => j.gemeinde);
             return ListView.builder(
-              itemCount: groupedData.keys.length,
+              itemCount: data.length,
               itemBuilder: (context, index) {
-                final gemeinde = groupedData.keys.elementAt(index);
-                final feuerwehren = groupedData[gemeinde];
-                return ExpansionTile(
-                  title: Text(gemeinde),
-                  children: feuerwehren!
-                      .map((f) => ListTile(
-                            title: Text(f.name),
-                            // subtitle: Text('Ort: ${f.ort}'),
-                          ))
-                      .toList(),
+                final gemeinde = data[index].gemeinde;
+                final ort = data[index].ort;
+                final name = data[index].name;
+                return ListTile(
+                  title: Text(name),
+                  subtitle: Text('Gemeinde/Stadt: $gemeinde'),
                 );
               },
             );
-            // if (snapshot.hasData) {
-            //   final data = snapshot.data!;
-            //   return ListView.builder(
-            //     itemCount: data.length,
-            //     itemBuilder: (context, index) {
-            //       final gemeinde = data[index].gemeinde;
-            //       final ort = data[index].ort;
-            //       final name = data[index].name;
-            //       return ListTile(
-            //         title: Text(name),
-            //         subtitle: Text('Gemeinde/Stadt: $gemeinde'),
-            //       );
-            //     },
-            //   );
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
           } else {
