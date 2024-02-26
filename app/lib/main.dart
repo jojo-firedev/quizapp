f6b209ad94ef02b76b484f01ba37ab8a5b8ce819
Jonas <jonas@firedev.tech>
2024-02-26 22:56:02 +0100

    Füge Buzzer-Konfigurationsseiten und Buzzer-Socket-Service hinzu

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/gobals.dart';
+import 'package:quizapp/presentation/einrichtung/buzzer_config/1_buzzer_paring_page.dart';
+import 'package:quizapp/presentation/einrichtung/buzzer_config/2_buzzer_config_page.dart';
+import 'package:quizapp/presentation/einrichtung/einrichtung_page.dart';
 import 'package:quizapp/presentation/einstellungen/einstellung_page.dart';
 import 'package:quizapp/presentation/home/home_page.dart';
 import 'package:quizapp/presentation/quiz/quiz_page.dart';
 import 'package:quizapp/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart';
+import 'package:quizapp/service/buzzer_socket_service.dart';
 import 'package:quizapp/style.dart';
 
 void main() {
+  Global.buzzerSocketService = BuzzerSocketService();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,9 +26,12 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       routes: {
         '/': (context) => const HomePage(),
+        '/einrichtung': (context) => const EinrichtungPage(),
+        '/einrichtung/buzzer_paring': (context) => const BuzzerParingPage(),
+        '/einrichtung/buzzer_config': (context) => const BuzzerConfigPage(),
         '/quiz': (context) => const QuizPage(),
-        '/teilnehmer': (context) => TeilnehmerUebersichtPage(),
-        '/einstellungen': (context) => EinstellungPage(),
+        '/teilnehmer': (context) => const TeilnehmerUebersichtPage(),
+        '/einstellungen': (context) => const EinstellungPage(),
       },
       initialRoute: '/',
     );
