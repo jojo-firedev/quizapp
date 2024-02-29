b65608b8c3bfa02ad099485eac3afa2c20ae1f3b
Jonas <jonas@firedev.tech>
2024-02-29 22:34:02 +0100

    cleanup code: use local udp port variable and jsonEncode for config message

--- app/lib/service/buzzer_udp_service.dart
+++ app/lib/service/buzzer_udp_service.dart
@@ -1,41 +1,30 @@
 import 'dart:convert';
-import 'dart:io';
-
 import 'package:udp/udp.dart';
 
 import 'package:quizapp/service/network_service.dart';
 
 class BuzzerUdpService {
   final NetworkService _networkService = NetworkService();
-  final Port buzzerUdpConfigPort = const Port(8090);
+  final Port buzzerUdpPort = const Port(8090);
+  final Port localUdpPort = const Port(8084);
 
   void sendUdpMessage(String message) async {
-    var sender = await UDP.bind(Endpoint.any(port: const Port(65000)));
-    await sender.send(
-        message.codeUnits, Endpoint.broadcast(port: buzzerUdpConfigPort));
+    List<int> data = utf8.encode(message);
+    var sender = await UDP.bind(Endpoint.any(port: localUdpPort));
+    await sender.send(data, Endpoint.broadcast(port: buzzerUdpPort));
     print('Sent config message');
   }
 
   void sendConfigWithIP() async {
     String ipAddress = await _networkService.getIpAddress();
-    const String broadcastAddress = '255.255.255.255'; // Broadcast address
-    const int port = 4444; // Port number
 
-    String configMessage =
-        '''{"Config": {"ServerIPAdresse": "192.168.0.120"}}''';
+    String configMessage = jsonEncode({
+      "Config": {"ServerIPAdresse": ipAddress}
+    });
+
     print('Config: $configMessage');
 
-    List<int> data = utf8.encode(configMessage);
-
-    var socket = await RawDatagramSocket.bind(InternetAddress.anyIPv4, 0);
-    var result = socket.send(data, InternetAddress(broadcastAddress), port);
-
-    var sender = await UDP.bind(Endpoint.any(port: const Port(65000)));
-    await sender.send(data, Endpoint.broadcast(port: buzzerUdpConfigPort));
-    print('Sent config message ${result}');
-
     sendUdpMessage(configMessage);
-    print('Sent config message');
   }
 
   void sendButtonLock() {
