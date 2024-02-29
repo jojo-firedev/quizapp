1c6eea16f01574ba04a17838b2fc58b8120a8e62
Jonas <jonas@firedev.tech>
2024-02-29 21:28:13 +0100

    add more udp communication

--- app/lib/service/buzzer_udp_service.dart
+++ app/lib/service/buzzer_udp_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:udp/udp.dart';
 
 import 'package:quizapp/service/network_service.dart';
@@ -6,15 +8,32 @@ class BuzzerUdpService {
   final NetworkService _networkService = NetworkService();
   final Port buzzerUdpConfigPort = const Port(8090);
 
+  void sendUdpMessage(String message) async {
+    var sender = await UDP.bind(Endpoint.any(port: const Port(65000)));
+    await sender.send(
+        message.codeUnits, Endpoint.broadcast(port: buzzerUdpConfigPort));
+    print('Sent config message');
+  }
+
   void sendConfigWithIP() async {
     String ipAddress = await _networkService.getIpAddress();
 
     String configMessage = '{"Config": {"ServerIPAdresse" : "$ipAddress"}}';
     print('Config: $configMessage');
 
-    var sender = await UDP.bind(Endpoint.any(port: const Port(65000)));
-    await sender.send(
-        configMessage.codeUnits, Endpoint.broadcast(port: buzzerUdpConfigPort));
+    sendUdpMessage(configMessage);
     print('Sent config message');
+  }
+
+  void sendButtonLock() {
+    String lockMessage = jsonEncode({'ButtonLock': []});
+
+    sendUdpMessage(lockMessage);
+  }
+
+  void sendButtonRelease() {
+    String releaseMessage = jsonEncode({'ButtonRelease': []});
+
+    sendUdpMessage(releaseMessage);
   }
 }
