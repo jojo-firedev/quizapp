c0d1ed76d2c0f5ec5aeb6fde7dc746fc6c7e237f
Jonas Hofses <jonas@firedev.tech>
2024-03-01 14:33:26 +0100

    Add quiz master page

---
+++ app/lib/presentation/quiz_master/quiz_master_page.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/service/buzzer_udp_service.dart';
+
+class QuizMasterPage extends StatefulWidget {
+  const QuizMasterPage({super.key});
+
+  @override
+  State<QuizMasterPage> createState() => _QuizMasterPageState();
+}
+
+class _QuizMasterPageState extends State<QuizMasterPage>
+    with SingleTickerProviderStateMixin {
+  BuzzerUdpService buzzerUdpService = BuzzerUdpService();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        children: [
+          const Column(
+            children: [
+              Text('Frage:'),
+              Text('Was ist der Anfangsbuchstabe vom ABC'),
+              SizedBox(height: 50),
+              Text('Antwort:'),
+              Text('Die Antwort könnte A sein')
+            ],
+          ),
+          Column(
+            children: [
+              const Text('JF Adendorf ist dran:'),
+              Row(
+                children: [
+                  Card(
+                    color: Colors.green,
+                    child: InkWell(
+                      onTap: () {},
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Icon(
+                                Icons.done,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width / 10,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                'Richtig',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.red,
+                    child: InkWell(
+                      onTap: () => buzzerUdpService.sendButtonRelease(),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Icon(
+                                Icons.close,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width / 10,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                'Falsch',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Card(
+                    color: Colors.grey.shade800,
+                    child: InkWell(
+                      onTap: () => buzzerUdpService.sendButtonLock(),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width / 10,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                'Lock all Buzzers',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    color: Colors.grey.shade800,
+                    child: InkWell(
+                      onTap: () => buzzerUdpService.sendButtonRelease(),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Icon(
+                                Icons.lock_open,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width / 10,
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text(
+                                'Unlock all Buzzers',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
