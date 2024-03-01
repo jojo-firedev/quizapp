a38870942ffbd3df603fcc2dbf27e2d2f750b897
Jonas Hofses <jonas@firedev.tech>
2024-03-01 14:38:48 +0100

    Add expanded with flex

--- app/lib/presentation/quiz_master/quiz_master_page.dart
+++ app/lib/presentation/quiz_master/quiz_master_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:quizapp/service/buzzer_udp_service.dart';
 
 class QuizMasterPage extends StatefulWidget {
@@ -17,135 +18,141 @@ class _QuizMasterPageState extends State<QuizMasterPage>
       appBar: AppBar(),
       body: Row(
         children: [
-          const Column(
-            children: [
-              Text('Frage:'),
-              Text('Was ist der Anfangsbuchstabe vom ABC'),
-              SizedBox(height: 50),
-              Text('Antwort:'),
-              Text('Die Antwort könnte A sein')
-            ],
+          Expanded(
+            flex: 2,
+            child: const Column(
+              children: [
+                Text('Frage:'),
+                Text('Was ist der Anfangsbuchstabe vom ABC'),
+                SizedBox(height: 50),
+                Text('Antwort:'),
+                Text('Die Antwort könnte A sein')
+              ],
+            ),
           ),
-          Column(
-            children: [
-              const Text('JF Adendorf ist dran:'),
-              Row(
-                children: [
-                  Card(
-                    color: Colors.green,
-                    child: InkWell(
-                      onTap: () {},
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Icon(
-                                Icons.done,
-                                color: Colors.white,
-                                size: MediaQuery.of(context).size.width / 10,
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                const Text('JF Adendorf ist dran:'),
+                Row(
+                  children: [
+                    Card(
+                      color: Colors.green,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                  size: MediaQuery.of(context).size.width / 10,
+                                ),
                               ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(20.0),
-                              child: Text(
-                                'Richtig',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
+                              const Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text(
+                                  'Richtig',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Card(
-                    color: Colors.red,
-                    child: InkWell(
-                      onTap: () => buzzerUdpService.sendButtonRelease(),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Icon(
-                                Icons.close,
-                                color: Colors.white,
-                                size: MediaQuery.of(context).size.width / 10,
+                    Card(
+                      color: Colors.red,
+                      child: InkWell(
+                        onTap: () => buzzerUdpService.sendButtonRelease(),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Icon(
+                                  Icons.close,
+                                  color: Colors.white,
+                                  size: MediaQuery.of(context).size.width / 10,
+                                ),
                               ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(20.0),
-                              child: Text(
-                                'Falsch',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
+                              const Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text(
+                                  'Falsch',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Card(
-                    color: Colors.grey.shade800,
-                    child: InkWell(
-                      onTap: () => buzzerUdpService.sendButtonLock(),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Icon(
-                                Icons.lock,
-                                color: Colors.white,
-                                size: MediaQuery.of(context).size.width / 10,
+                  ],
+                ),
+                Row(
+                  children: [
+                    Card(
+                      color: Colors.grey.shade800,
+                      child: InkWell(
+                        onTap: () => buzzerUdpService.sendButtonLock(),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Icon(
+                                  Icons.lock,
+                                  color: Colors.white,
+                                  size: MediaQuery.of(context).size.width / 10,
+                                ),
                               ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(20.0),
-                              child: Text(
-                                'Lock all Buzzers',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
+                              const Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text(
+                                  'Lock all Buzzers',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Card(
-                    color: Colors.grey.shade800,
-                    child: InkWell(
-                      onTap: () => buzzerUdpService.sendButtonRelease(),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: Icon(
-                                Icons.lock_open,
-                                color: Colors.white,
-                                size: MediaQuery.of(context).size.width / 10,
+                    Card(
+                      color: Colors.grey.shade800,
+                      child: InkWell(
+                        onTap: () => buzzerUdpService.sendButtonRelease(),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Icon(
+                                  Icons.lock_open,
+                                  color: Colors.white,
+                                  size: MediaQuery.of(context).size.width / 10,
+                                ),
                               ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(20.0),
-                              child: Text(
-                                'Unlock all Buzzers',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20),
+                              const Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text(
+                                  'Unlock all Buzzers',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
