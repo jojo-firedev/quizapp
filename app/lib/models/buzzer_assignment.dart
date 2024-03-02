d3562fb00c6d581dbf08392a56e72a387a003d75
Jonas <jonas@firedev.tech>
2024-03-02 23:21:16 +0100

    Fügt die BuzzerAssignment-Klasse hinzu

---
+++ app/lib/models/buzzer_assignment.dart
@@ -0,0 +1,11 @@
+class BuzzerAssignment {
+  final String name;
+  final String gemeinde;
+  final String mac;
+
+  BuzzerAssignment({
+    required this.name,
+    required this.gemeinde,
+    required this.mac,
+  });
+}
