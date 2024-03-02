35d1dc1f20f28ae52783fa61c79ec1ec4588f9fe
Jonas <jonas@firedev.tech>
2024-03-02 13:14:19 +0100

    Remove global BuzzerSocketService

--- app/lib/globals.dart
+++ app/lib/globals.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
 import 'package:logger/logger.dart';
-import 'package:quizapp/service/buzzer_socket_service.dart';
 
 class Global {
   static List<Socket> sockets = [];
   static List<String> macs = [];
-  static BuzzerSocketService? buzzerSocketService;
+  // static BuzzerSocketService? buzzerSocketService;
   static Logger logger = Logger(
     printer: PrettyPrinter(),
     level: Level.debug, // Set the logging level
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/globals.dart';
 import 'package:quizapp/presentation/einrichtung/buzzer_config/buzzer_paring_page.dart';
 import 'package:quizapp/presentation/einrichtung/buzzer_config/buzzer_assignment_page.dart';
 import 'package:quizapp/presentation/einrichtung/einrichtung_page.dart';
@@ -8,11 +7,9 @@ import 'package:quizapp/presentation/home/home_page.dart';
 import 'package:quizapp/presentation/quiz/quiz_page.dart';
 import 'package:quizapp/presentation/quiz_master/quiz_master_page.dart';
 import 'package:quizapp/presentation/teilnehmer_uebersicht/teilnehmer_uebersicht_page.dart';
-import 'package:quizapp/service/buzzer_socket_service.dart';
 import 'package:quizapp/style.dart';
 
 void main() {
-  Global.buzzerSocketService = BuzzerSocketService();
   runApp(const MyApp());
 }
 
