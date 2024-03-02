27cbaf99795ff7187f31bff989e984562dcde4b4
Jonas <jonas@firedev.tech>
2024-03-02 23:21:58 +0100

    Aktualisiere BuzzerManagerService Klasse und füge listenToStream() hinzu

--- app/lib/service/buzzer_manager_service.dart
+++ app/lib/service/buzzer_manager_service.dart
@@ -1,83 +1,129 @@
-import 'dart:convert';
+import 'dart:async';
 
 import 'package:quizapp/globals.dart';
+import 'package:quizapp/models/buzzer_assignment.dart';
 import 'package:quizapp/service/buzzer_socket_service.dart';
 import 'package:quizapp/service/buzzer_udp_listener_service.dart';
 import 'package:quizapp/service/buzzer_udp_service.dart';
 
 class BuzzerManagerService {
-  BuzzerType buzzerType = BuzzerType.udp;
-  bool _isBuzzerLocked = false;
   late BuzzerUdpService buzzerUdpService;
   late BuzzerUdpListenerService buzzerUdpListenerService;
   late BuzzerSocketService buzzerSocketService;
 
-  BuzzerManagerService(this.buzzerType) {
-    setup();
-  }
-
-  bool get isBuzzerLocked => _isBuzzerLocked;
-
-  set isBuzzerLocked(bool value) {
-    _isBuzzerLocked = value;
-  }
+  BuzzerManagerService();
 
   void setup() {
-    if (buzzerType == BuzzerType.socket) {
+    if (Global.buzzerType == BuzzerType.socket) {
       buzzerSocketService = BuzzerSocketService();
-    } else if (buzzerType == BuzzerType.udp) {
+    } else if (Global.buzzerType == BuzzerType.udp) {
       buzzerUdpService = BuzzerUdpService();
       buzzerUdpListenerService = BuzzerUdpListenerService();
     }
   }
 
+  void listenToStream() {
+    Global.streamController.stream.listen((event) {
+      Map<String, dynamic> jsonObject = event;
+
+      switch (Global.connectionMode) {
+        case ConnectionMode.idle:
+          break;
+        case ConnectionMode.parring:
+          String mac = jsonObject.keys.first;
+          if (!Global.macs.contains(mac)) {
+            Global.macs.add(mac);
+            Global.logger.d('Added mac: $mac');
+          }
+          Global.logger.d(Global.macs);
+          BuzzerUdpService().sendBuzzerRelease();
+        case ConnectionMode.assignment:
+          break;
+        case ConnectionMode.game:
+          switch (jsonObject.values.first) {
+            case 'Connected':
+              String mac = jsonObject.keys.first;
+              if (!Global.macs.contains(mac)) {
+                Global.macs.add(mac);
+              }
+              break;
+            case 'ButtonPressed':
+              String mac = jsonObject.keys.first;
+              if (!Global.macs.contains(mac)) {
+                Global.macs.add(mac);
+              }
+              BuzzerUdpService().sendBuzzerLock(winnerMac: mac);
+              break;
+            default:
+          }
+        default:
+          break;
+      }
+    });
+  }
+
   void sendConfig() {
-    buzzerUdpService.sendConfigWithIP();
+    buzzerUdpService.sendConfig();
+  }
+
+  void connectAllBuzzer() {
+    Global.connectionMode = ConnectionMode.parring;
+    sendBuzzerRelease();
   }
 
   void sendBuzzerLock() {
-    if (buzzerType == BuzzerType.socket) {
+    if (Global.buzzerType == BuzzerType.socket) {
       buzzerSocketService.sendBuzzerLock();
-    } else if (buzzerType == BuzzerType.udp) {
+    } else if (Global.buzzerType == BuzzerType.udp) {
       buzzerUdpService.sendBuzzerLock();
     }
   }
 
   void sendBuzzerRelease() {
-    if (buzzerType == BuzzerType.socket) {
+    if (Global.buzzerType == BuzzerType.socket) {
       buzzerSocketService.sendBuzzerRelease();
-    } else if (buzzerType == BuzzerType.udp) {
+    } else if (Global.buzzerType == BuzzerType.udp) {
       buzzerUdpService.sendBuzzerRelease();
     }
   }
 
   void sendPing() {
-    if (buzzerType == BuzzerType.socket) {
+    if (Global.buzzerType == BuzzerType.socket) {
       buzzerSocketService.sendPing();
-    } else if (buzzerType == BuzzerType.udp) {
+    } else if (Global.buzzerType == BuzzerType.udp) {
       buzzerUdpService.sendPing();
     }
   }
 
-  void handleMessage(data, ip, port, internalbuzzerType) {
-    String message = utf8.decode(data);
-    Map<String, dynamic> jsonObject = jsonDecode(message);
+  void assignBuzzer({required String name, required String gemeinde}) {
+    print('Start assigning buzzer for $name');
+    StreamSubscription<Map<String, dynamic>> subscription =
+        Global.streamController.stream.listen((event) async {});
 
-    Global.logger.d('Received message from $ip:$port: $message');
+    subscription.onData((data) {
+      String mac = data.keys.first;
 
-    if (jsonObject.values.first == 'Connected') {
-      String mac = jsonObject.keys.first;
-      Global.macs.add(mac);
-    } else if (jsonObject.values.first == 'ButtonPressed') {
-      print('Button pressed');
-      String mac = jsonObject.keys.first;
-      if (internalbuzzerType == BuzzerType.socket) {
-        buzzerSocketService.sendBuzzerLock(winnerMac: mac);
-      } else if (internalbuzzerType == BuzzerType.udp) {
-        buzzerUdpService.sendBuzzerLock(winnerMac: mac);
-      }
-    }
+      assignBuzzerToJugendfeuerwehr(name: name, gemeinde: gemeinde, mac: mac);
+      subscription.cancel();
+    });
+  }
+
+  void assignBuzzerToJugendfeuerwehr(
+      {required String name, required String gemeinde, required String mac}) {
+    print('Assigne Buzzer $mac to $name');
+    Global.assignedBuzzer.add(BuzzerAssignment(
+      name: name,
+      gemeinde: gemeinde,
+      mac: mac,
+    ));
   }
 }
 
 enum BuzzerType { udp, socket, silent }
+
+enum ConnectionMode {
+  idle,
+  parring,
+  assignment,
+  game,
+}
