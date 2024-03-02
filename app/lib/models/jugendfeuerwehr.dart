67af364d3eee9bc0a020872b4619d19e454cccc8
Jonas <jonas@firedev.tech>
2024-03-02 23:21:21 +0100

    Entferne das Feld "ort" aus der Jugendfeuerwehr-Klasse

--- app/lib/models/jugendfeuerwehr.dart
+++ app/lib/models/jugendfeuerwehr.dart
@@ -1,11 +1,9 @@
 class Jugendfeuerwehr {
   final String name;
-  final String ort;
   final String gemeinde;
 
   Jugendfeuerwehr({
     required this.name,
-    required this.ort,
     required this.gemeinde,
   });
 }
