bcc52f907c5480600b9b387e8a2a1ad6ce0e90ea
Jonas <jonas@firedev.tech>
2024-03-02 23:21:26 +0100

    Füge Buzzer-Manager-Service hinzu und aktualisiere Buzzer-Zuordnungsseite

--- app/lib/presentation/einrichtung/buzzer_config/buzzer_assignment_page.dart
+++ app/lib/presentation/einrichtung/buzzer_config/buzzer_assignment_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/globals.dart';
 import 'package:quizapp/models/jugendfeuerwehr.dart';
+import 'package:quizapp/service/buzzer_manager_service.dart';
 import 'package:quizapp/service/csv_service.dart';
 
 class BuzzerAssignmentPage extends StatefulWidget {
@@ -13,31 +15,55 @@ class _BuzzerAssignmentPageState extends State<BuzzerAssignmentPage> {
   final CsvService csvService = const CsvService();
 
   @override
+  void initState() {
+    Global.connectionMode = ConnectionMode.assignment;
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Buzzer zuordnen'),
+        actions: const [
+          Text('Tippe auf das Icon und betätige dann den Buzzer.'),
+          SizedBox(width: 10)
+        ],
       ),
-      body: FutureBuilder<List<Jugendfeuerwehr>>(
-        future: csvService.readCsv(),
+      body: StreamBuilder(
+        stream: Global.streamController.stream,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            final data = snapshot.data!;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                final gemeinde = data[index].gemeinde;
-                final name = data[index].name;
-                return ListTile(
-                  title: Text(name),
-                  subtitle: Text(gemeinde),
-                );
+            return FutureBuilder<List<Jugendfeuerwehr>>(
+              future: csvService.readCsv(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final data = snapshot.data!;
+                  return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final gemeinde = data[index].gemeinde;
+                      final name = data[index].name;
+                      return ListTile(
+                        title: Text(name),
+                        subtitle: Text(gemeinde),
+                        trailing: const Icon(Icons.link, color: Colors.red),
+                        onTap: () {
+                          BuzzerManagerService()
+                              .assignBuzzer(name: name, gemeinde: gemeinde);
+                        },
+                      );
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  return const CircularProgressIndicator();
+                }
               },
             );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
           } else {
-            return const CircularProgressIndicator();
+            return Container();
           }
         },
       ),
