190fbb0e3585e528f8e516af6fa1d4aa17ccc011
Jonas <jonas@firedev.tech>
2024-03-02 23:21:31 +0100

    Aktualisiere BuzzerParingPage mit BuzzerManagerService

--- app/lib/presentation/einrichtung/buzzer_config/buzzer_paring_page.dart
+++ app/lib/presentation/einrichtung/buzzer_config/buzzer_paring_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/globals.dart';
-import 'package:quizapp/service/buzzer_udp_service.dart';
+import 'package:quizapp/service/buzzer_manager_service.dart';
 
 class BuzzerParingPage extends StatefulWidget {
   const BuzzerParingPage({super.key});
@@ -10,12 +9,6 @@ class BuzzerParingPage extends StatefulWidget {
 }
 
 class _BuzzerParingPageState extends State<BuzzerParingPage> {
-  @override
-  void initState() {
-    // Global.buzzerSocketService!.updateConnectedSocketsCount();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,65 +21,66 @@ class _BuzzerParingPageState extends State<BuzzerParingPage> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const Text(
+                'Schließe alle Buzzer an die Stromversorgung an und drücke auf "Buzzer konfigurieren".'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+              ),
+              onPressed: () => BuzzerManagerService().sendConfig(),
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Buzzer konfigurieren',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+                "Die Buzzer sind verbunden, wenn die LED im Buzzer dauerhaft Lila leuchtet."),
+            const Text(
+                'Sobald alle Buzzer lila leuchten, drücke auf "Verbinden".'),
+            const SizedBox(height: 20),
             ElevatedButton(
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.red),
               ),
               onPressed: () {
                 setState(() {
-                  BuzzerUdpService().sendConfigWithIP();
-                  Global.printSockets();
+                  BuzzerManagerService().connectAllBuzzer();
                 });
               },
               child: const Padding(
                 padding: EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.send_time_extension,
-                      color: Colors.white,
-                      // size: MediaQuery.of(context).size.width / 10,
-                      size: 50,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'Buzzer konfigurieren',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                  ],
+                child: Text(
+                  'Verbinden',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
                 ),
               ),
             ),
-            const SizedBox(height: 10),
-            const Text(
-                'Schließen Sie alle Buzzer an die Stromversorgung an und drücke Sie auf "Buzzer konfigurieren".'),
-            // Container(
-            //   padding: const EdgeInsets.all(20),
-            //   child: StreamBuilder<int>(
-            //     // stream: Global.buzzerSocketService!.connectedSocketsCountStream,
-            //     builder: (context, snapshot) {
-            //       if (snapshot.hasData) {
-            //         return Text(
-            //           'Verbundene Buzzer: ${snapshot.data}',
-            //           style: const TextStyle(
-            //               fontSize: 30, fontWeight: FontWeight.bold),
-            //         );
-            //       } else {
-            //         return const CircularProgressIndicator();
-            //       }
-            //     },
-            //   ),
-            // ),
             const SizedBox(height: 20),
             const Text(
-                "Wenn alle Buzzer verbunden sind, drücken Sie auf 'Weiter'."),
+                'Sobald die Buzzer weiß leuchten, müssen alle Buzzer nacheinander betätigt werden.'),
             const SizedBox(height: 20),
-            FilledButton(
+            const Text(
+                'Wenn alle Buzzer betätigt wurden, drücke auf "Weiter".'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.red),
+              ),
               onPressed: () {
                 Navigator.pushNamed(context, '/einrichtung/buzzer_assignment');
               },
-              child: const Text('Weiter'),
+              child: const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text(
+                  'Weiter',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
             ),
           ],
         ),
