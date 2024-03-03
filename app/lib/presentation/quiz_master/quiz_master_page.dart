0a013e128819b9ca7eab19fdae4f1de65c5c3d04
Jonas <jonas@firedev.tech>
2024-03-03 14:20:22 +0100

    Update desing for buttons on quiz master

--- app/lib/presentation/quiz_master/quiz_master_page.dart
+++ app/lib/presentation/quiz_master/quiz_master_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:quizapp/service/buzzer_udp_service.dart';
 
 class QuizMasterPage extends StatefulWidget {
@@ -34,121 +35,159 @@ class _QuizMasterPageState extends State<QuizMasterPage>
             child: Column(
               children: [
                 const Text('JF Adendorf ist dran:'),
-                Row(
-                  children: [
-                    Card(
-                      color: Colors.green,
-                      child: InkWell(
-                        onTap: () {},
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Icon(
-                                  Icons.done,
-                                  color: Colors.white,
-                                  size: MediaQuery.of(context).size.width / 10,
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(20),
+                        width: MediaQuery.of(context).size.width / 4,
+                        height: MediaQuery.of(context).size.height / 3,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: InkWell(
+                          onTap: () {},
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                    size:
+                                        MediaQuery.of(context).size.width / 10,
+                                  ),
                                 ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  'Richtig',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
+                                const Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text(
+                                    'Richtig',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Card(
-                      color: Colors.red,
-                      child: InkWell(
-                        onTap: () => buzzerUdpService.sendBuzzerRelease(),
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Icon(
-                                  Icons.close,
-                                  color: Colors.white,
-                                  size: MediaQuery.of(context).size.width / 10,
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(20),
+                        width: MediaQuery.of(context).size.width / 4,
+                        height: MediaQuery.of(context).size.height / 3,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: InkWell(
+                          onTap: () => buzzerUdpService.sendBuzzerRelease(),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.close,
+                                    color: Colors.white,
+                                    size:
+                                        MediaQuery.of(context).size.width / 10,
+                                  ),
                                 ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  'Falsch',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
+                                const Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text(
+                                    'Falsch',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-                Row(
-                  children: [
-                    Card(
-                      color: Colors.grey.shade800,
-                      child: InkWell(
-                        onTap: () => buzzerUdpService.sendBuzzerLock(),
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Icon(
-                                  Icons.lock,
-                                  color: Colors.white,
-                                  size: MediaQuery.of(context).size.width / 10,
+                Expanded(
+                  flex: 2,
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(20),
+                        width: MediaQuery.of(context).size.width / 4,
+                        height: MediaQuery.of(context).size.height / 3,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade800,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: InkWell(
+                          onTap: () => buzzerUdpService.sendBuzzerLock(),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.lock,
+                                    color: Colors.white,
+                                    size:
+                                        MediaQuery.of(context).size.width / 10,
+                                  ),
                                 ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  'Lock all Buzzers',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
+                                const Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text(
+                                    'Lock all Buzzers',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Card(
-                      color: Colors.grey.shade800,
-                      child: InkWell(
-                        onTap: () => buzzerUdpService.sendBuzzerRelease(),
-                        child: Center(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Icon(
-                                  Icons.lock_open,
-                                  color: Colors.white,
-                                  size: MediaQuery.of(context).size.width / 10,
+                      Container(
+                        margin: const EdgeInsets.all(20),
+                        padding: const EdgeInsets.all(20),
+                        width: MediaQuery.of(context).size.width / 4,
+                        height: MediaQuery.of(context).size.height / 3,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade800,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: InkWell(
+                          onTap: () => buzzerUdpService.sendBuzzerRelease(),
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: Icon(
+                                    Icons.lock_open,
+                                    color: Colors.white,
+                                    size:
+                                        MediaQuery.of(context).size.width / 10,
+                                  ),
                                 ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.all(20.0),
-                                child: Text(
-                                  'Unlock all Buzzers',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 20),
+                                const Padding(
+                                  padding: EdgeInsets.all(20.0),
+                                  child: Text(
+                                    'Unlock all Buzzers',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
