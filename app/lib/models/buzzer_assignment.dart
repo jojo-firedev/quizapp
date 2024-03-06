64155f2166418beb6e509c37efacd5afc50c5821
Jonas <jonas@firedev.tech>
2024-03-06 19:38:43 +0100

    update models with toJson and fromJson

--- app/lib/models/buzzer_assignment.dart
+++ app/lib/models/buzzer_assignment.dart
@@ -1,11 +1,25 @@
 class BuzzerAssignment {
-  final String name;
-  final String gemeinde;
+  final int tisch;
   final String mac;
 
   BuzzerAssignment({
-    required this.name,
-    required this.gemeinde,
+    required this.tisch,
     required this.mac,
   });
+
+  @override
+  String toString() {
+    return 'BuzzerAssignment{tisch: $tisch, mac: $mac}';
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'tisch': tisch,
+      'mac': mac,
+    };
+  }
+
+  factory BuzzerAssignment.fromJson(Map<String, dynamic> json) {
+    return BuzzerAssignment(tisch: json['tisch'], mac: json['mac']);
+  }
 }
--- app/lib/models/jugendfeuerwehr.dart
+++ app/lib/models/jugendfeuerwehr.dart
@@ -1,9 +1,31 @@
 class Jugendfeuerwehr {
+  final int reihnfolge;
   final String name;
   final String gemeinde;
+  final int tisch;
 
   Jugendfeuerwehr({
+    required this.reihnfolge,
     required this.name,
     required this.gemeinde,
+    required this.tisch,
   });
+
+  factory Jugendfeuerwehr.fromJson(Map<String, dynamic> json) {
+    return Jugendfeuerwehr(
+      reihnfolge: json['reihnfolge'],
+      name: json['name'],
+      gemeinde: json['gemeinde'],
+      tisch: json['tisch'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'reihnfolge': reihnfolge,
+      'name': name,
+      'gemeinde': gemeinde,
+      'tisch': tisch,
+    };
+  }
 }
