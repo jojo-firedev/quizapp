7c8f1c89ecbd8d14e4f1403a5b5af3e3b17a6950
Jonas <jonas@firedev.tech>
2024-03-06 19:37:26 +0100

    File manager service migrate from csv service, use json as base communication

---
+++ app/lib/service/file_manager_service.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:quizapp/models/buzzer_assignment.dart';
+import 'package:quizapp/models/jugendfeuerwehr.dart';
+
+class FileManagerService {
+  const FileManagerService();
+
+  Future<List<Jugendfeuerwehr>> readAllJFsFromJson() async {
+    final jsonString =
+        await File('assets/data/jugendfeuerwehren.json').readAsString();
+    final jfJson = jsonDecode(jsonString);
+    final jf = jfJson
+        .map<Jugendfeuerwehr>((e) => Jugendfeuerwehr.fromJson(e))
+        .toList();
+    return jf;
+  }
+
+  void saveBuzzerAssignmentToJson(List<BuzzerAssignment> buzzerAssignment) {
+    final buzzerAssignmentJson =
+        buzzerAssignment.map((e) => e.toJson()).toList();
+    final buzzerAssignmentString = jsonEncode(buzzerAssignmentJson);
+    File('assets/data/buzzer_assignment.json')
+        .writeAsString(buzzerAssignmentString);
+  }
+
+  Future<List<BuzzerAssignment>> readBuzzerAssignmentFromJson() async {
+    final jsonString =
+        await File('assets/data/buzzer_assignment.json').readAsString();
+    final buzzerAssignmentJson = jsonDecode(jsonString);
+    final buzzerAssignment = buzzerAssignmentJson
+        .map<BuzzerAssignment>((e) => BuzzerAssignment.fromJson(e))
+        .toList();
+    return buzzerAssignment;
+  }
+}
