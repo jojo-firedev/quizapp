b337843f3aef8ab22e1da8faf1de0a2a32ccf15e
Jonas <jonas@firedev.tech>
2024-03-06 21:05:15 +0100

    some test buttons on settings page

--- app/lib/presentation/einstellungen/einstellung_page.dart
+++ app/lib/presentation/einstellungen/einstellung_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/models/jugendfeuerwehr.dart';
+import 'package:quizapp/service/file_manager_service.dart';
 
 class EinstellungPage extends StatefulWidget {
   const EinstellungPage({super.key});
@@ -8,6 +10,8 @@ class EinstellungPage extends StatefulWidget {
 }
 
 class _EinstellungPageState extends State<EinstellungPage> {
+  FileManagerService fileManagerService = const FileManagerService();
+
   @override
   void initState() {
     super.initState();
@@ -19,9 +23,19 @@ class _EinstellungPageState extends State<EinstellungPage> {
       appBar: AppBar(
         title: const Text('Einstellungen'),
       ),
-      body: const Center(
-        child: Text("Einstellungen"),
-      ),
+      body: Center(
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () => fileManagerService.saveJFsToJson([
+              Jugendfeuerwehr(reihenfolge: 1, name: 'Test', tisch: 1),
+              Jugendfeuerwehr(reihenfolge: 2, name: 'Test2', tisch: 2),
+              Jugendfeuerwehr(reihenfolge: 3, name: 'pwerigj', tisch: 3),
+            ]),
+            child: Text('Save JF to CSV'),
+          )
+        ],
+      )),
     );
   }
 }
