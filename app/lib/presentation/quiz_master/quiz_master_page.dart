f636b8806b9d494f2c818b1b8501d455761fc25e
Jonas <jonas@firedev.tech>
2024-03-06 19:39:20 +0100

    update quiz master page with new component for CustomButton

--- app/lib/presentation/quiz_master/quiz_master_page.dart
+++ app/lib/presentation/quiz_master/quiz_master_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quizapp/service/buzzer_udp_service.dart';
+import 'package:quizapp/globals.dart';
+import 'package:quizapp/service/buzzer_manager_service.dart';
 
 class QuizMasterPage extends StatefulWidget {
   const QuizMasterPage({super.key});
@@ -10,22 +11,74 @@ class QuizMasterPage extends StatefulWidget {
 
 class _QuizMasterPageState extends State<QuizMasterPage>
     with SingleTickerProviderStateMixin {
-  BuzzerUdpService buzzerUdpService = BuzzerUdpService();
+  @override
+  void initState() {
+    Global.connectionMode = ConnectionMode.game;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Row(
+      appBar: AppBar(title: const Text('JF Adendorf ist dran')),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
         children: [
           const Expanded(
             flex: 2,
             child: Column(
               children: [
-                Text('Frage:'),
-                Text('Was ist der Anfangsbuchstabe vom ABC'),
-                SizedBox(height: 50),
-                Text('Antwort:'),
-                Text('Die Antwort könnte A sein')
+                Text(
+                  'Frage:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Was ist der Anfangsbuchstabe vom ABC',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Antwort:',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Die Antwort könnte A sein',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      'Runde für:',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'JF Adendorf',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text(
+                      'Gedrückt:',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '-',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
@@ -33,165 +86,104 @@ class _QuizMasterPageState extends State<QuizMasterPage>
             flex: 3,
             child: Column(
               children: [
-                const Text('JF Adendorf ist dran:'),
                 Expanded(
                   flex: 2,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(20),
-                        padding: const EdgeInsets.all(20),
-                        width: MediaQuery.of(context).size.width / 4,
-                        height: MediaQuery.of(context).size.height / 3,
-                        decoration: BoxDecoration(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        CustomButton(
+                          icon: Icons.done,
                           color: Colors.green,
-                          borderRadius: BorderRadius.circular(20),
+                          // text: 'Richtig',
+                          text: '',
+                          onPressed: () {},
                         ),
-                        child: InkWell(
-                          onTap: () {},
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  child: Icon(
-                                    Icons.done,
-                                    color: Colors.white,
-                                    size:
-                                        MediaQuery.of(context).size.width / 10,
-                                  ),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.all(20.0),
-                                  child: Text(
-                                    'Richtig',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(20),
-                        padding: const EdgeInsets.all(20),
-                        width: MediaQuery.of(context).size.width / 4,
-                        height: MediaQuery.of(context).size.height / 3,
-                        decoration: BoxDecoration(
+                        CustomButton(
+                          icon: Icons.close,
                           color: Colors.red,
-                          borderRadius: BorderRadius.circular(20),
+                          // text: 'Falsch',
+                          text: '',
+                          onPressed: () {},
                         ),
-                        child: InkWell(
-                          onTap: () => buzzerUdpService.sendBuzzerRelease(),
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  child: Icon(
-                                    Icons.close,
-                                    color: Colors.white,
-                                    size:
-                                        MediaQuery.of(context).size.width / 10,
-                                  ),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.all(20.0),
-                                  child: Text(
-                                    'Falsch',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(20),
-                        padding: const EdgeInsets.all(20),
-                        width: MediaQuery.of(context).size.width / 4,
-                        height: MediaQuery.of(context).size.height / 3,
-                        decoration: BoxDecoration(
+                        CustomButton(
+                          icon: Icons.lock,
                           color: Colors.grey.shade800,
-                          borderRadius: BorderRadius.circular(20),
+                          text: '',
+                          onPressed: () =>
+                              Global.buzzerManagerService.sendBuzzerLock(),
                         ),
-                        child: InkWell(
-                          onTap: () => buzzerUdpService.sendBuzzerLock(),
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  child: Icon(
-                                    Icons.lock,
-                                    color: Colors.white,
-                                    size:
-                                        MediaQuery.of(context).size.width / 10,
-                                  ),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.all(20.0),
-                                  child: Text(
-                                    'Lock all Buzzers',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(20),
-                        padding: const EdgeInsets.all(20),
-                        width: MediaQuery.of(context).size.width / 4,
-                        height: MediaQuery.of(context).size.height / 3,
-                        decoration: BoxDecoration(
+                        CustomButton(
+                          icon: Icons.lock_open,
                           color: Colors.grey.shade800,
-                          borderRadius: BorderRadius.circular(20),
+                          text: '',
+                          onPressed: () =>
+                              Global.buzzerManagerService.sendBuzzerRelease(),
                         ),
-                        child: InkWell(
-                          onTap: () => buzzerUdpService.sendBuzzerRelease(),
-                          child: Center(
-                            child: Column(
-                              children: [
-                                Expanded(
-                                  child: Icon(
-                                    Icons.lock_open,
-                                    color: Colors.white,
-                                    size:
-                                        MediaQuery.of(context).size.width / 10,
-                                  ),
-                                ),
-                                const Padding(
-                                  padding: EdgeInsets.all(20.0),
-                                  child: Text(
-                                    'Unlock all Buzzers',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.icon,
+    required this.color,
+    required this.text,
+    required this.onPressed,
+  });
+
+  final IconData icon;
+  final Color color;
+  final String text;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(20),
+      width: MediaQuery.of(context).size.width / 4.5,
+      height: MediaQuery.of(context).size.height / 5,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        onTap: () => onPressed(),
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: MediaQuery.of(context).size.width / 10,
+                ),
+              ),
+              if (text.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Text(
+                    text,
+                    style: const TextStyle(color: Colors.white, fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
