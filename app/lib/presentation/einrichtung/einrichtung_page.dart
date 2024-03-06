6aa059bd2b145f107f0f23925e7ce19f5abe2cca
Jonas <jonas@firedev.tech>
2024-03-06 22:32:05 +0100

    Aktualisiere Einrichtungsseite mit neuen Funktionen und button for open quiz page

--- app/lib/presentation/einrichtung/einrichtung_page.dart
+++ app/lib/presentation/einrichtung/einrichtung_page.dart
@@ -9,26 +9,40 @@ class EinrichtungPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Einrichtung'),
       ),
-      body: ListView(
+      body: Column(
         children: [
-          ListTile(
-            title: const Text('Jugendfeuerwehren importieren'),
-            subtitle: const Text(
-                'Jugendfeuerwehren für den Wettbewerb aus JSON importieren'),
-            onTap: () =>
-                Navigator.of(context).pushNamed('/einrichtung/jf_assignment'),
+          Expanded(
+            child: ListView(
+              children: [
+                ListTile(
+                  title: const Text('Jugendfeuerwehren importieren'),
+                  subtitle: const Text(
+                      'Jugendfeuerwehren für den Wettbewerb aus JSON importieren'),
+                  onTap: () => Navigator.of(context)
+                      .pushNamed('/einrichtung/jf_assignment'),
+                ),
+                ListTile(
+                  title: const Text('Buzzer verbinden'),
+                  subtitle:
+                      const Text('Verbindung mit allen Buzzer herstellen'),
+                  onTap: () => Navigator.of(context)
+                      .pushNamed('/einrichtung/buzzer_paring'),
+                ),
+                ListTile(
+                  title: const Text('Buzzer zuordnen'),
+                  subtitle: const Text('Buzzer den Jugendfeuerwehren zuordnen'),
+                  onTap: () => Navigator.of(context)
+                      .pushNamed('/einrichtung/buzzer_assignment'),
+                ),
+              ],
+            ),
           ),
           ListTile(
-            title: const Text('Buzzer verbinden'),
-            subtitle: const Text('Verbindung mit allen Buzzer herstellen'),
-            onTap: () =>
-                Navigator.of(context).pushNamed('/einrichtung/buzzer_paring'),
-          ),
-          ListTile(
-            title: const Text('Buzzer zuordnen'),
-            subtitle: const Text('Buzzer den Jugendfeuerwehren zuordnen'),
-            onTap: () => Navigator.of(context)
-                .pushNamed('/einrichtung/buzzer_assignment'),
+            onTap: () => Navigator.of(context).popAndPushNamed('/quiz-master'),
+            title: const Text('Quiz starten',
+                style: TextStyle(color: Colors.white)),
+            tileColor: Colors.red,
+            trailing: const Icon(Icons.arrow_forward, color: Colors.white),
           ),
         ],
       ),
