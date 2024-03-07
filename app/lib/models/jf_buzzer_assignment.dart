b3c0c0244e6c9afa15dbb3526571263db929ff85
Jonas Hofses <jonas@firedev.tech>
2024-03-07 13:55:33 +0100

    new model for jf buzzer assignment

---
+++ app/lib/models/jf_buzzer_assignment.dart
@@ -0,0 +1,12 @@
+import 'package:quizapp/models/buzzer_assignment.dart';
+import 'package:quizapp/models/jugendfeuerwehr.dart';
+
+class JfBuzzerAssignment {
+  final Jugendfeuerwehr jugendfeuerwehr;
+  final BuzzerAssignment buzzerAssignment;
+
+  JfBuzzerAssignment({
+    required this.jugendfeuerwehr,
+    required this.buzzerAssignment,
+  });
+}
