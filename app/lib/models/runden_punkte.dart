faac438cdd5be9c34017fb97b927d4ed2bc2a524
Jonas <jonas@firedev.tech>
2024-03-11 22:05:30 +0100

    first layout of model for RundenPunkte

---
+++ app/lib/models/runden_punkte.dart
@@ -0,0 +1,12 @@
+class RundenPunkte {
+  int startPunkte;
+  int gesetztePunkte;
+  List<int> erhaltenePunkte;
+  late int ausgangsPunkte;
+
+  RundenPunkte({
+    required this.startPunkte,
+    required this.gesetztePunkte,
+    required this.erhaltenePunkte,
+  });
+}
