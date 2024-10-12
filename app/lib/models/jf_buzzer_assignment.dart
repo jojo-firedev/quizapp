ae121c5a68152e409901c4a09ca0dd107459fb9f
Jonas <jonas@firedev.tech>
2024-10-13 01:00:06 +0200

    add logPointsPerCategory

--- app/lib/models/jf_buzzer_assignment.dart
+++ app/lib/models/jf_buzzer_assignment.dart
@@ -1,11 +1,29 @@
+import 'package:quizapp/globals.dart';
 import 'package:quizapp/models/buzzer_assignment.dart';
 import 'package:quizapp/models/jugendfeuerwehr.dart';
-import 'package:quizapp/models/punkte.dart';
+import 'package:quizapp/models/points.dart';
 
 class JfBuzzerAssignment {
   final Jugendfeuerwehr jugendfeuerwehr;
   final BuzzerAssignment buzzerAssignment;
-  List<Punkte> punkte = [];
+  List<Points> points = [];
+
+  /// Logging method for debugging purposes
+  void logPointsPerCategory(int kategorieReihenfolge) {
+    final Points point = points
+        .where(
+            (element) => element.kategorieReihenfolge == kategorieReihenfolge)
+        .first;
+
+    Global.logger.d(
+      'jugendfeuerwehr: ${jugendfeuerwehr.name}\n'
+      'gesetztePunkte: ${point.gesetztePunkte}\n'
+      'erhaltenePunkte: ${point.erhaltenePunkte}\n'
+      'endpunkte: ${point.endpunkte}',
+    );
+  }
+
+  int get gesamtPunkte => points.fold(0, (a, b) => a + b.endpunkte);
 
   JfBuzzerAssignment({
     required this.jugendfeuerwehr,
