2180b950c02ce496fe1ecb0a66bf2272ab77cbc3
Jonas <jonas@firedev.tech>
2024-10-13 00:59:40 +0200

    rename points file

--- app/lib/models/points.dart
+++ app/lib/models/points.dart
@@ -1,12 +1,11 @@
-class Punkte {
+class Points {
   int kategorieReihenfolge;
   int gesetztePunkte;
   List<int> erhaltenePunkte = [];
 
-  int get endpunkte =>
-      gesetztePunkte + erhaltenePunkte.fold(0, (a, b) => a + b);
+  int get endpunkte => erhaltenePunkte.fold(0, (a, b) => a + b);
 
-  Punkte({
+  Points({
     required this.kategorieReihenfolge,
     required this.gesetztePunkte,
   });
