0d5ddfd7e8b818eca7300c32ad3e0c99cea2ebf0
Jonas Hofses <jonas.hofses@dbl-marwitz.de>
2024-10-16 10:55:06 +0200

    rename main app

--- screen_app/lib/main.dart
+++ screen_app/lib/main.dart
@@ -4,24 +4,24 @@ import 'package:quizapp_screen/bloc/screen_app_bloc.dart';
 import 'package:quizapp_screen/presentation/main_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ScreenApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ScreenApp extends StatelessWidget {
+  const ScreenApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quizapp',
+      title: 'Screen Quizapp',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: BlocProvider(
-        create: (context) => ScreenAppBloc(),
-        child: MainPage(),
+        create: (context) => ScreenAppBloc()..add(ConnectToServer()),
+        child: const MainPage(),
       ),
     );
   }
