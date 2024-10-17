207b19464399ddfac7513731a618a73e590e1445
Jonas Hofses <jonas.hofses@dbl-marwitz.de>
2024-10-17 08:32:03 +0200

    use specific path on linux for raspi deployment

--- app/lib/service/file_manager_service.dart
+++ app/lib/service/file_manager_service.dart
@@ -12,14 +12,28 @@ class FileManagerService {
 
   // Generische Methode zum Speichern von JSON-Dateien
   Future<void> _saveToFile<T>(
-      String path, List<T> data, Function(T) toJson) async {
+    String fileName,
+    List<T> data,
+    Function(T) toJson,
+  ) async {
+    String path = './assets/data/';
+    if (Platform.isLinux) {
+      path = '/home/pi/quizapp/data/';
+    }
     final jsonString = jsonEncode(data.map((e) => toJson(e)).toList());
     await File(path).writeAsString(jsonString);
   }
 
   // Generische Methode zum Lesen von JSON-Dateien
   Future<List<T>> _readFromFile<T>(
-      String path, T Function(Map<String, dynamic>) fromJson) async {
+    String fileName,
+    T Function(Map<String, dynamic>) fromJson,
+  ) async {
+    String path = './assets/data/';
+    if (Platform.isLinux) {
+      path = '/home/pi/quizapp/data/';
+    }
+
     try {
       final jsonString = await File(path).readAsString();
       final jsonData = jsonDecode(jsonString) as List;
@@ -31,29 +45,26 @@ class FileManagerService {
   }
 
   Future<void> saveJFs(List<Jugendfeuerwehr> jf) async {
-    await _saveToFile(
-        './assets/data/jugendfeuerwehren.json', jf, (e) => e.toJson());
+    await _saveToFile('jugendfeuerwehren.json', jf, (e) => e.toJson());
   }
 
   Future<List<Jugendfeuerwehr>> readJFs() async {
-    return _readFromFile(
-        './assets/data/jugendfeuerwehren.json', Jugendfeuerwehr.fromJson);
+    return _readFromFile('jugendfeuerwehren.json', Jugendfeuerwehr.fromJson);
   }
 
   Future<void> saveBuzzerAssignment(
       List<BuzzerAssignment> buzzerAssignment) async {
-    await _saveToFile('./assets/data/buzzer_assignment.json', buzzerAssignment,
-        (e) => e.toJson());
+    await _saveToFile(
+        'buzzer_assignment.json', buzzerAssignment, (e) => e.toJson());
   }
 
   Future<List<BuzzerAssignment>> readBuzzerAssignment() async {
-    return _readFromFile(
-        'assets/data/buzzer_assignment.json', BuzzerAssignment.fromJson);
+    return _readFromFile('buzzer_assignment.json', BuzzerAssignment.fromJson);
   }
 
   Future<FragenList> readFragen() async {
     try {
-      final jsonString = await File('assets/data/fragen.json').readAsString();
+      final jsonString = await File('fragen.json').readAsString();
       return FragenList.fromJson(jsonDecode(jsonString));
     } catch (e) {
       print("Error reading Fragen: $e");
@@ -63,29 +74,29 @@ class FileManagerService {
 
   Future<void> saveFragen(FragenList fragenList) async {
     final fragenString = fragenList.toRawJson();
-    await File('assets/data/fragen.json').writeAsString(fragenString);
+    await File('fragen.json').writeAsString(fragenString);
   }
 
   // Save Points to a file
   Future<void> savePoints(List<Points> points) async {
-    await _saveToFile('assets/data/points.json', points, (e) => e.toJson());
+    await _saveToFile('points.json', points, (e) => e.toJson());
   }
 
   // Read Points from a file
   Future<List<Points>> readPoints() async {
-    return _readFromFile('assets/data/points.json', Points.fromJson);
+    return _readFromFile('points.json', Points.fromJson);
   }
 
   // Save JfBuzzerAssignment list to a JSON file
   Future<void> saveJfBuzzerAssignments(
       List<JfBuzzerAssignment> assignments) async {
-    await _saveToFile('assets/data/jf_buzzer_assignments.json', assignments,
-        (e) => e.toJson());
+    await _saveToFile(
+        'jf_buzzer_assignments.json', assignments, (e) => e.toJson());
   }
 
   // Read JfBuzzerAssignment list from a JSON file
   Future<List<JfBuzzerAssignment>> readJfBuzzerAssignments() async {
     return _readFromFile(
-        'assets/data/jf_buzzer_assignments.json', JfBuzzerAssignment.fromJson);
+        'jf_buzzer_assignments.json', JfBuzzerAssignment.fromJson);
   }
 }
