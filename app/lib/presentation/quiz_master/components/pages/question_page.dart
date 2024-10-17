fa426f58efe6b9dd58824d5748734b2ba1a11b19
Jonas <jonas.hofses@dbl-marwitz.de>
2024-10-17 17:36:17 +0200

    remove app bar

--- app/lib/presentation/quiz_master/components/pages/question_page.dart
+++ app/lib/presentation/quiz_master/components/pages/question_page.dart
@@ -14,234 +14,247 @@ class QuestionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('${state.currentJf} ist dran')),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
+      body: Stack(
         children: [
-          Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                const Text(
-                  'Frage:',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  state.frage.frage,
-                  style: const TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-                const SizedBox(height: 20),
-                const Text(
-                  'Antwort:',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  state.frage.antwort,
-                  style: const TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+          Positioned(
+            top: 5,
+            left: 5,
+            child: IconButton(
+              icon: Icon(Icons.navigate_before),
+              onPressed: () => Navigator.of(context).pop(),
             ),
           ),
-          Expanded(
-            flex: 1,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
+          Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
                   children: [
                     const Text(
-                      'Runde für:',
+                      'Frage:',
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                     Text(
-                      state.currentJf,
+                      state.frage.frage,
                       style: const TextStyle(fontSize: 20),
                       textAlign: TextAlign.center,
                     ),
-                  ],
-                ),
-                Column(
-                  children: [
+                    const SizedBox(height: 20),
                     const Text(
-                      'Gedrückt:',
+                      'Antwort:',
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                     Text(
-                      state.pressedJf,
+                      state.frage.antwort,
                       style: const TextStyle(fontSize: 20),
                       textAlign: TextAlign.center,
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          'Runde für:',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          state.currentJf,
+                          style: const TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          'Gedrückt:',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          state.pressedJf,
+                          style: const TextStyle(fontSize: 20),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              if (state is QuizMasterQuestionConfirmShowAnswer)
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CustomButton(
+                                icon: Icons.done,
+                                color: Colors.green,
+                                text: 'Antwort anzeigen',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(ShowAnswer()),
+                              ),
+                              CustomButton(
+                                icon: Icons.fast_forward,
+                                color: Colors.blue,
+                                text: 'Nächste Frage',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(ShowNextQuestion()),
+                              ),
+                              CustomButton(
+                                icon: Icons.lock,
+                                color: Colors.grey.shade800,
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(LockAllBuzzers()),
+                              ),
+                              CustomButton(
+                                icon: Icons.lock_open,
+                                color: Colors.grey.shade800,
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(ReleaseAllBuzzers()),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              else if (state is QuizMasterQuestionShowAnswer)
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CustomButton(
+                                icon: Icons.fast_forward,
+                                color: Colors.blue,
+                                text: 'Nächste Frage',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(ShowNextQuestion()),
+                              ),
+                              // CustomButton(
+                              //   icon: Icons.close,
+                              //   color: Colors.red,
+                              //   text: 'Antwort nicht anzeigen',
+                              //   onPressed: () => context
+                              //       .read<QuizMasterBloc>()
+                              //       .add(WrongAnswer()),
+                              // ),
+                              CustomButton(
+                                icon: Icons.lock,
+                                color: Colors.grey.shade800,
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(LockAllBuzzers()),
+                              ),
+                              CustomButton(
+                                icon: Icons.lock_open,
+                                color: Colors.grey.shade800,
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(ReleaseAllBuzzers()),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              else
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CustomButton(
+                                icon: Icons.done,
+                                color: Colors.green,
+                                // text: 'Richtig',
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(CorrectAnswer()),
+                              ),
+                              CustomButton(
+                                icon: Icons.close,
+                                color: Colors.red,
+                                // text: 'Falsch',
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(WrongAnswer()),
+                              ),
+                              CustomButton(
+                                icon: Icons.lock,
+                                color: Colors.grey.shade800,
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(LockAllBuzzers()),
+                              ),
+                              CustomButton(
+                                icon: Icons.lock_open,
+                                color: Colors.grey.shade800,
+                                text: '',
+                                onPressed: () => context
+                                    .read<QuizMasterBloc>()
+                                    .add(ReleaseAllBuzzers()),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+            ],
           ),
-          if (state is QuizMasterQuestionConfirmShowAnswer)
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          CustomButton(
-                            icon: Icons.done,
-                            color: Colors.green,
-                            text: 'Antwort anzeigen',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(ShowAnswer()),
-                          ),
-                          CustomButton(
-                            icon: Icons.fast_forward,
-                            color: Colors.blue,
-                            text: 'Nächste Frage',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(ShowNextQuestion()),
-                          ),
-                          CustomButton(
-                            icon: Icons.lock,
-                            color: Colors.grey.shade800,
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(LockAllBuzzers()),
-                          ),
-                          CustomButton(
-                            icon: Icons.lock_open,
-                            color: Colors.grey.shade800,
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(ReleaseAllBuzzers()),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          else if (state is QuizMasterQuestionShowAnswer)
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          CustomButton(
-                            icon: Icons.fast_forward,
-                            color: Colors.blue,
-                            text: 'Nächste Frage',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(ShowNextQuestion()),
-                          ),
-                          // CustomButton(
-                          //   icon: Icons.close,
-                          //   color: Colors.red,
-                          //   text: 'Antwort nicht anzeigen',
-                          //   onPressed: () => context
-                          //       .read<QuizMasterBloc>()
-                          //       .add(WrongAnswer()),
-                          // ),
-                          CustomButton(
-                            icon: Icons.lock,
-                            color: Colors.grey.shade800,
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(LockAllBuzzers()),
-                          ),
-                          CustomButton(
-                            icon: Icons.lock_open,
-                            color: Colors.grey.shade800,
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(ReleaseAllBuzzers()),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          else
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          CustomButton(
-                            icon: Icons.done,
-                            color: Colors.green,
-                            // text: 'Richtig',
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(CorrectAnswer()),
-                          ),
-                          CustomButton(
-                            icon: Icons.close,
-                            color: Colors.red,
-                            // text: 'Falsch',
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(WrongAnswer()),
-                          ),
-                          CustomButton(
-                            icon: Icons.lock,
-                            color: Colors.grey.shade800,
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(LockAllBuzzers()),
-                          ),
-                          CustomButton(
-                            icon: Icons.lock_open,
-                            color: Colors.grey.shade800,
-                            text: '',
-                            onPressed: () => context
-                                .read<QuizMasterBloc>()
-                                .add(ReleaseAllBuzzers()),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
         ],
       ),
     );
